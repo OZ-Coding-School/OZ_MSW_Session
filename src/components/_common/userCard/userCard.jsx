9e444fdcd56fcf9268e75e5ee3edeb5ca2753fe7
강성구 <sungku2757@gmail.com>
2025-06-10 10:56:50 +0900

    ✨ Feat: usercard added
    usercard added

---
+++ src/components/_common/userCard/userCard.jsx
@@ -0,0 +1,12 @@
+import { UserIcon } from "@heroicons/react/24/outline";
+
+export const UserCard = ({ userInfo }) => {
+  return (
+    <div className="flex items-center justify-between gap-2 my-1 ml-1">
+      <div className="w-6 h-6 flex items-center justify-center rounded-full bg-secondary">
+        <UserIcon className="w-4 h-4" />
+      </div>
+      <div>{userInfo.username}</div>
+    </div>
+  );
+};

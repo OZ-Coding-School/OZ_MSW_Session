a891961f0794afca52be4823869b2241febdf185
강성구 <sungku2757@gmail.com>
2025-06-10 16:42:39 +0900

    ♻️ Refactor: usercard render minimized
    refactor: usercard render minimized

--- src/components/_common/userCard/userCard.jsx
+++ src/components/_common/userCard/userCard.jsx
@@ -1,6 +1,7 @@
 import { UserIcon } from "@heroicons/react/24/outline";
+import { memo } from "react";
 
-export const UserCard = ({ userInfo }) => {
+export const UserCard = memo(({ userInfo }) => {
   return (
     <div className="flex items-center justify-between gap-2 my-1 ml-1">
       <div className="w-6 h-6 flex items-center justify-center rounded-full bg-secondary">
@@ -9,4 +10,4 @@ export const UserCard = ({ userInfo }) => {
       <div>{userInfo.username}</div>
     </div>
   );
-};
+});

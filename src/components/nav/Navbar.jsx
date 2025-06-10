aaec73e45579a7e4feb2d75e4e6a363ca95cd0ee
강성구 <sungku2757@gmail.com>
2025-06-10 10:58:21 +0900

    ✨ Feat: login button action added
    modal action, button action added

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,9 +1,15 @@
 import FullLogo from "../../assets/logos/FullLogo.png";
-import Button from "../_common/buttons/Button";
+import { useModal } from "../../hooks/useModal";
+import { CustomButton } from "../_common/buttons";
+import { CustomModal } from "../_common/modal";
+
+import LoginForm from "../login/Login.Form";
 
 export const Navbar = () => {
+  const LoginModal = useModal();
+
   return (
-    <nav className="fixed top-0 right-0 left-0 w-full h-20 flex justify-between items-center px-10 shadow-md z-50 bg-white">
+    <nav className="fixed top-0 right-0 left-0 w-full h-20 flex justify-between items-center px-10 shadow-md bg-white">
       <div className="h-full py-3">
         <img
           src={FullLogo}
@@ -13,7 +19,14 @@ export const Navbar = () => {
           className="h-full w-full object-fit"
         />
       </div>
-      <Button mode="button">로그인</Button>
+      <CustomButton mode="button" onClick={() => LoginModal.setIsOpen(true)}>
+        로그인
+      </CustomButton>
+      {LoginModal.isOpen && (
+        <CustomModal onClose={() => LoginModal.setIsOpen(false)}>
+          <LoginForm onClose={() => LoginModal.setIsOpen(false)} />
+        </CustomModal>
+      )}
     </nav>
   );
 };
